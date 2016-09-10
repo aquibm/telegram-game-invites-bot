210dcef68d6fd3ccf4097276970f3e5dbce988d9
Aquib Master <aquibmaster@gmail.com>
2016-09-10 13:14:44 +1200

    Setup base structure and npm scripts to build

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _nodeTelegramBotApi = require('node-telegram-bot-api');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Bot = function Bot() {
+    _classCallCheck(this, Bot);
+
+    console.log('Hello world!');
+};
+
+new Bot();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { TelegramBot } from 'node-telegram-bot-api';
+
+class Bot {
+    constructor() {
+        console.log('Hello world!');
+    }
+}
+
+new Bot();
