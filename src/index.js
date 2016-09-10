f404f76961b4e4c59ac52ee698035ac3603089de
Aquib Master <aquibmaster@gmail.com>
2016-09-10 18:07:53 +1200

    Split out babel config. Introduce eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import { TelegramBot } from 'node-telegram-bot-api';
 
-class Bot {
-    constructor() {
-        console.log('Hello world!');
-    }
-}
-
-new Bot();
+const bot = new TelegramBot();
