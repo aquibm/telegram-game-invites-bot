f8930bbcedd8c033806aa1c8663bf87014fc26a7
Aquib Master <aquibmaster@gmail.com>
2016-09-10 18:26:57 +1200

    Setup unit tests

---
+++ src/index.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('bot suite', () => {
+    it('should always pass', () => {
+        expect(true).to.be.true;
+    });
+
+    it('should always pass as well', () => {
+        expect(false).to.be.false;
+    });
+});
